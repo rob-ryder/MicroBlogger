eb1ca933f31e71a054c46bd58166598cd432d911
Rob Ryder <rob.ryder@sage.com>
2014-08-19 11:03:28 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,25 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+
+# MicroBlogger::Application.config.secret_token = '84a078509fdfd8889de4820a535f6bdae7d16eea801663d4b1727bf51f5b557c70f0d845bafbc34b3d7dbe4be8d8a576357f47267b98a2b9329acbe81df27dd8'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+MicroBlogger::Application.config.secret_key_base = secure_token
