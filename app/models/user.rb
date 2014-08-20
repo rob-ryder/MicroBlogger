0c3e4c266fc84c85a26adc8aa5177afb00755523
Rob Ryder <rob.ryder@sage.com>
2014-08-20 15:31:14 +0100

    Enhance user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
