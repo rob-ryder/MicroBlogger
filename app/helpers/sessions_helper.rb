51edbe72cb1952d787bece35e780c7494d9f7867
Rob Ryder <rob.ryder@sage.com>
2014-08-22 11:43:27 +0100

    Add friendly redirect on sign-in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,5 +29,14 @@ module SessionsHelper
   def signed_in?
     !current_user.nil?
   end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
   
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -43,6 +43,7 @@ describe "Authentication" do
     
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
+
       describe "in the Users controller" do
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
@@ -53,6 +54,21 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",    with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+        describe "after signing in" do
+          it "should render the desired protected page" do
+            expect(page).to have_title('Edit user')
+          end
+        end
+      end
+      
     end
 
     describe "as wrong user" do
