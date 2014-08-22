06a8c4fb5090b507510d6a16e43d1bdf886c829a
Rob Ryder <rob.ryder@sage.com>
2014-08-22 11:13:59 +0100

    Add account settings with authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
+  
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+  
   def new
     @user = User.new
   end
+  
   def show
     @user = User.find(params[:id])
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -16,10 +22,33 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  # before filters
+  
+  def signed_in_user
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
   
 end
