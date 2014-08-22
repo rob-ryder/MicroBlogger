4168a0118f67a0c95456d684e54b01ea54e4edb2
Rob Ryder <rob.ryder@sage.com>
2014-08-22 17:15:14 +0100

    Add micropost to users view, enhance populate task

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,5 @@ class Micropost < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   attr_accessible :content, :user_id
   validates :user_id, presence: true
+  validates :content, presence: true, length: {maximum: 140} 
 end
