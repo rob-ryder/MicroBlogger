cc71d925cc8b26ccbe1e135d26a47daead5dceb1
Rob Ryder <rob.ryder@sage.com>
2014-08-22 15:21:22 +0100

    Complete users functionality, including admin delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   
   before_filter :signed_in_user, only: [:edit, :update, :index]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: [:destroy]
   
   def new
     @user = User.new
@@ -38,6 +39,13 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
   
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+    flash[:success] = "User '#{user.name}' has been deleted."
+    redirect_to users_path
+  end
+  
   private
 
   def user_params
@@ -50,6 +58,12 @@ class UsersController < ApplicationController
     unless signed_in?
       store_location
       redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def admin_user
+    unless signed_in? && current_user.admin?
+      redirect_to(root_url)
     end
   end
   
